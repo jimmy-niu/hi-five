29b142b19c158cdfe55c8c8f320fc7c0bb73247c
Jimmy Niu <chenenniu@gmail.com>
2020-01-26 10:34:13 -0500

    Added CSS styles to the friend page

--- public/linkFriend/index.js
+++ public/linkFriend/index.js
@@ -1,32 +1,61 @@
 window.onload = () => {
-  const friendCode = window.location.href.split('=')[1]
-  const friendLink = document.querySelector('#friend-link')
+    const friendCode = window.location.href.split("=")[1];
+    const friendLink = document.querySelector("#friend-link");
 
-  // Login to Firebase
-  firebase.auth().signInAnonymously().catch(function(error) {
-    alert(error.message)
-  })
+    // Login to Firebase
+    firebase
+        .auth()
+        .signInAnonymously()
+        .catch(function(error) {
+            alert(error.message);
+        });
 
-  firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-      if (friendCode) {
-        // Add friend groups
-        const db = firebase.firestore()
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            if (friendCode) {
+                // Add friend groups
+                const db = firebase.firestore();
 
-        Promise.all([
-          db.collection("groups").doc(user.uid).set({
-            partnerID: friendCode
-          }),
-          db.collection("groups").doc(friendCode).set({
-            partnerID: user.uid
-          })
-        ])
-        .then(() => friendLink.innerText = 'Friend added successfully')
-        .catch(() => friendLink.innerText = 'An error occured connecting with friend')
-      } else {
-        // Generate friend link
-        friendLink.innerText = window.location.href + `?code=${user.uid}`
-      }
-    }
-  })
-}
+                Promise.all([
+                    db
+                        .collection("groups")
+                        .doc(user.uid)
+                        .set({
+                            partnerID: friendCode
+                        }),
+                    db
+                        .collection("groups")
+                        .doc(friendCode)
+                        .set({
+                            partnerID: user.uid
+                        })
+                ])
+                    .then(() => (friendLink.innerText = "Friend added successfully"))
+                    .catch(
+                        () => (friendLink.innerText = "An error occured connecting with friend")
+                    );
+            } else {
+                // Generate friend link
+                friendLink.innerText = window.location.href + `?code=${user.uid}`;
+            }
+        }
+    });
+
+    const friendCodeElement = document.getElementById("friend-link");
+    friendCodeElement.onclick = function() {
+        console.log("clicking!");
+        copyToClipboard(friendCodeElement.innerText);
+    };
+};
+
+const copyToClipboard = str => {
+    const el = document.createElement("textarea");
+    el.value = str;
+    el.setAttribute("readonly", "");
+    el.style.position = "absolute";
+    el.style.left = "-9999px";
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand("copy");
+    document.body.removeChild(el);
+};
